bb25ff682e3074c16c5d39c3c061236d6f53711d
jakebroffman <jake.broffman1088@gmail.com>
2023-05-10 18:11:29 -0400

    add icons to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,16 +7,19 @@ function NavBar() {
             <br></br>
             <div className="home-link">
             <NavLink style={{ marginRight: "10px" }} to="/">
+                <img className="navBar-icon" alt="house icon" src="http://www.clipartbest.com/cliparts/dcr/6oj/dcr6ojkoi.png"/>
                 Home
             </NavLink>
             </div>
             <div className="auction-link">
             <NavLink style={{ marginRight: "10px" }} to="/art">
+                <img className="art-icon" alt="paint pallet icon" src="http://www.clipartbest.com/cliparts/xig/LRo/xigLRo6pT.png"/>
                 Art for Sale
             </NavLink>
             </div>
             <div className="seller-link">
             <NavLink style={{ marginRight: "10px" }} to="/sellers">
+                <img className="navBar-icon" alt="gavel" src="http://www.clipartbest.com/cliparts/nTX/pAo/nTXpAo7Mc.png" />
                 List a Work to Sell
             </NavLink>
             </div>
