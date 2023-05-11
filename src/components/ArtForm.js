22d961124fa57765db86357465674db9346caf55
jakebroffman <jake.broffman1088@gmail.com>
2023-05-10 20:25:57 -0400

    add medium input to artForm component

--- src/components/ArtForm.js
+++ src/components/ArtForm.js
@@ -1,15 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "semantic-ui-react"
 
-function ArtForm( {
-    handleArtistChange,
-    formData,
-    handleTitleChange,
-    handleImageChange,
-    handlePriceChange,
-    handleSubmit
+function ArtForm( { 
+    artForAuction,
+    setArtForAuction
 } ) {
     
+  const [formData, setFormData] = useState({
+        artist: "",
+        medium: "",
+        image: "",
+        openingPrice: "",
+    })
+
+    function handleArtistChange(e) {
+        console.log(e.target.value)
+        setFormData({
+            ...formData,
+            artist: e.target.value
+        });
+    }
+
+    function handleTitleChange(e) {
+        setFormData({
+            ...formData,
+            title: e.target.value
+        });
+    }
+
+    function handleImageChange(e) {
+        setFormData({
+            ...formData,
+            image: e.target.value
+        });
+    }
+
+    function handlePriceChange(e) {
+        setFormData({
+            ...formData,
+            openingPrice: e.target.value
+        });
+    }
+
+    function handleMediumChange(e) {
+        setFormData({
+            ...formData,
+            medium: e.target.value
+        });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        fetch("http://localhost:3000/art", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify(formData)
+        })
+        .then((r) =>r.json())
+        .then((newArt) => handleNewArt(newArt))
+    }
+
+    function handleNewArt(newArt) {
+        setArtForAuction([...artForAuction, newArt])
+    }
+
     return (
         <div className="form-box">
             <h1>List A Work For Auction:</h1>
@@ -46,6 +102,14 @@ function ArtForm( {
                         name="Asking Price"
                         onChange={handlePriceChange}
                         value={formData.openingPrice}
+                    />
+                    <Form.Input
+                        fluid
+                        label="Medium"
+                        placeholder="Medium"
+                        name="Medium"
+                        onChange={handleMediumChange}
+                        value={formData.medium}
                     />
                 </Form.Group>
                 <Form.Button>Submit</Form.Button>
